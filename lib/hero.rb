89b6c75da01d79fabcec2227e9a284eb6a0132cf
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-03 20:35:47 -0300

    second test: testing attributes

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,7 +1,8 @@
 class Hero
-  attr_accessor :weapon
+  attr_accessor :weapon, :hp
 
   def initialize
     @weapon = 'sword'
+    @hp = 1200
   end
 end
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -6,4 +6,10 @@ describe Hero do
     hero = Hero.new
     expect(hero.weapon).to eq('sword')
   end
+
+  it 'has more than 1000 hp points' do
+    hero = Hero.new
+    expect(hero.hp).to be > 1000
+    # expect([hero.hp]).to all(be > 1000)
+  end
 end
