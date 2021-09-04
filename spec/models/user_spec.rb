0143f0a794bfff7caca94dafcf683ae06103a507
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-04 04:38:13 -0300

    using gem faker to feed the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is invalid if the lvl is not between 1 and 99' do
-    expect(User.create(nickname: 'chronos', kind: :wizard, level: 100)).to_not be_valid
+    nickname = Faker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = rand(100..999)
+    user = User.new(nickname: nickname, kind: kind, level: level)
+
+    expect(user).to_not be_valid
   end
+
   it 'return the correct hero title' do
-    user = User.create(nickname: 'Asura', kind: :wizard, level: 1)
-    expect(user.title).to eq('wizard Asura #1')
+    nickname = Faker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = rand(1..99)
+    user = User.create(nickname: nickname, kind: kind, level: level)
+
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
