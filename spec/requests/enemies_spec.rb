92f02fcf3b5cbadf2a7ef07248f558e31b415e6d
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-05 10:17:15 -0300

    testing the api destroy

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -29,11 +29,41 @@ RSpec.describe 'Enemies', type: :request do
 
     context 'when the enemy does not exist' do
       it 'return status code 404' do
-        delete '/enemies/1'
+        attributes = attributes_for(:enemy)
+        put '/enemies/1', params: attributes
         expect(response).to have_http_status(404)
       end
       it 'returns a not found message' do
-        delete '/enemies/1'
+        attributes = attributes_for(:enemy)
+        put '/enemies/1', params: attributes
+        expect(response.body).to include('Couldn\'t find Enemy')
+      end
+    end
+  end
+
+  describe 'DELETE /enemies' do
+    context 'when the enemy exists' do
+      it 'returns status code 200' do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+        expect(response).to have_http_status(204)
+      end
+
+      it 'destroy the record' do
+        enemy = create(:enemy)
+        delete "/enemies/#{enemy.id}"
+        expect { enemy.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'when the enemy does not exist' do
+      it 'returns status code 404' do
+        delete '/enemies/0'
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        delete '/enemies/0'
         expect(response.body).to include('Couldn\'t find Enemy')
       end
     end
